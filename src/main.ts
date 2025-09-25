31dca06b17e686be0b05814833d68a55228ade7b
ebotzocm <ebotzocm@miumg.edu.gt>
2025-09-24 23:20:52 -0600

    Carpeta 16 iniciada

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
 import { RpcCustomExceptionFilter } from './common';
 
@@ -11,7 +11,12 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', {
+    exclude: [{
+      path: '',
+      method: RequestMethod.GET,
+    }]
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -24,7 +29,7 @@ async function bootstrap() {
 
   await app.listen(envs.port);
 
-  console.log("Hola mundo-Tercer cambio");
+  console.log("Health Check configured");
 
   logger.log(`Gateway running on port ${envs.port}`);
 }
